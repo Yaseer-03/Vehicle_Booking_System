3abd507b32fbb17ad673c059af74ccffd11a2836
yaseer_03 <96855590+Yaseer-03@users.noreply.github.com>
2023-09-29 22:31:17 +0530

    Add files via upload

---
+++ VehicleBookingSystem.java
@@ -0,0 +1,233 @@
+package CoreJavaProject;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Vehicle {
+    public String registrationNumber;
+    public String type;
+    public String manufacturer;
+    public int capacity;
+    public boolean isAvailable;
+    public double distanceTravelled;
+    public int totalTrips;
+
+    public Vehicle(String registrationNumber, String type, String manufacturer, int capacity) {
+        this.registrationNumber = registrationNumber;
+        this.type = type;
+        this.manufacturer = manufacturer;
+        this.capacity = capacity;
+        this.isAvailable = true;
+        this.distanceTravelled = 0.0;
+        this.totalTrips = 0;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void allocate(double distance) {
+        isAvailable = false;
+        distanceTravelled += distance;
+        totalTrips++;
+    }
+
+    public void deallocate() {
+        isAvailable = true;
+    }
+
+    public double getTotalDistance() {
+        return distanceTravelled;
+    }
+
+    public int getTotalTrips() {
+        return totalTrips;
+    }
+}
+
+class Trip {
+    public int tripId;
+    public String vehicleRegistrationNumber;
+    public double startKilometer;
+    public double endKilometer;
+    public String startTime;
+    public String endTime;
+    public String status;
+
+    public Trip(int tripId, String vehicleRegistrationNumber, double startKilometer, double endKilometer,
+            String startTime) {
+        this.tripId = tripId;
+        this.vehicleRegistrationNumber = vehicleRegistrationNumber;
+        this.startKilometer = startKilometer;
+        this.endKilometer = endKilometer;
+        this.startTime = startTime;
+        this.status = "In Progress";
+    }
+
+    public void completeTrip(String endTime) {
+        this.endTime = endTime;
+        this.status = "Completed";
+    }
+
+    public Object getStatus() {
+        return null;
+    }
+
+    public String getTripId() {
+        return null;
+    }
+}
+
+public class VehicleBookingSystem {
+    public static List<Vehicle> vehicles = new ArrayList<>();
+    public static List<Trip> trips = new ArrayList<>();
+    public static ExecutorService executor = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter 1 for Vehicle Registration");
+            System.out.println("Enter 2 for Vehicle Allocation Request");
+            System.out.println("Enter 3 for Multiple Allocation Requests in Parallel");
+            System.out.println("Enter 4 for Vehicle Usage Report");
+            System.out.println("Enter 0 to exit");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    registerVehicle(scanner);
+                    break;
+                case 2:
+                    allocateVehicle(scanner);
+                    break;
+                case 3:
+                    parallelAllocation(scanner);
+                    break;
+                case 4:
+                    generateCsvReport();
+                    break;
+                case 0:
+                    executor.shutdown();
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    public static void registerVehicle(Scanner scanner) {
+        System.out.print("Enter Registration Number: ");
+        String registrationNumber = scanner.nextLine();
+        System.out.print("Enter Vehicle Type: ");
+        String type = scanner.nextLine();
+        System.out.print("Enter Vehicle Manufacturer: ");
+        String manufacturer = scanner.nextLine();
+        System.out.print("Enter Vehicle Capacity: ");
+        int capacity = scanner.nextInt();
+        scanner.nextLine();
+
+        Vehicle vehicle = new Vehicle(registrationNumber, type, manufacturer, capacity);
+        vehicles.add(vehicle);
+
+        System.out.println("Vehicle registered successfully.");
+    }
+
+    public static void allocateVehicle(Scanner scanner) {
+        System.out.print("Enter Trip ID: ");
+        int tripId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter Start Kilometer: ");
+        double startKilometer = scanner.nextDouble();
+        System.out.print("Enter End Kilometer: ");
+        double endKilometer = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Enter Start Time: ");
+        String startTime = scanner.nextLine();
+        System.out.print("Enter Required Vehicle Capacity: ");
+        int requiredCapacity = scanner.nextInt();
+        scanner.nextLine();
+
+        boolean allocated = false;
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.isAvailable() && vehicle.getCapacity() >= requiredCapacity) {
+                double distance = endKilometer - startKilometer;
+                vehicle.allocate(distance);
+                Trip trip = new Trip(tripId, vehicle.getRegistrationNumber(), startKilometer, endKilometer, startTime);
+                trips.add(trip);
+                allocated = true;
+                System.out.println("Trip " + tripId + " allocated to vehicle " + vehicle.getRegistrationNumber());
+                break;
+            }
+        }
+
+        if (!allocated) {
+            System.out.println("No available vehicles for trip " + tripId + ". Retrying after 30 seconds...");
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            allocateVehicle(scanner);
+        }
+    }
+
+    public static void parallelAllocation(Scanner scanner) {
+        List<Callable<Void>> tasks = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            tasks.add(() -> {
+                allocateVehicle(scanner);
+                return null; // Callable requires a return value
+            });
+        }
+
+        try {
+            executor.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void generateCsvReport() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date now = new Date(0);
+        String currentTime = dateFormat.format(now);
+
+        System.out.println("Generating CSV report...");
+        System.out.println("Registration Number, Vehicle Type, Total Trips Completed, Total Distance Traveled");
+
+        for (Vehicle vehicle : vehicles) {
+            System.out.println(vehicle.getRegistrationNumber() + "," + vehicle.getType() + ","
+                    + vehicle.getTotalTrips() + "," + vehicle.getTotalDistance());
+        }
+
+        for (Trip trip : trips) {
+            if ("In Progress".equals(trip.getStatus())) {
+                // Complete ongoing trips
+                trip.completeTrip(currentTime);
+                System.out.println("Trip " + trip.getTripId() + " completed.");
+            }
+        }
+    }
+}
