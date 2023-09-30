314ba41c96d847005d4376715d5c0e92850d9a12
yaseer_03 <96855590+Yaseer-03@users.noreply.github.com>
2023-09-30 14:32:17 +0530

    Trip.java

---
+++ Trip.java
@@ -0,0 +1,62 @@
+class Trip {
+    public int tripId;
+    public String vehicleRegistrationNumber;
+    public double startKilometer;
+    public double endKilometer;
+    public String startTime;
+    public String endTime;
+    public String status;
+    public String allocatedVehicleRegistrationNumber;
+
+    public Trip(int tripId, String vehicleRegistrationNumber, double startKilometer, double endKilometer,
+                String startTime) {
+        this.tripId = tripId;
+        this.vehicleRegistrationNumber = vehicleRegistrationNumber;
+        this.startKilometer = startKilometer;
+        this.endKilometer = endKilometer;
+        this.startTime = startTime;
+        this.status = "In Progress";
+    }
+    public void setAllocatedVehicle(String registrationNumber) {
+        this.allocatedVehicleRegistrationNumber = registrationNumber;
+    }
+
+    public String getAllocatedVehicleRegistrationNumber() {
+        return allocatedVehicleRegistrationNumber;
+    }
+    public Vehicle findVehicleByRegistrationNumber(String registrationNumber) {
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.getRegistrationNumber().equals(registrationNumber)) {
+                return vehicle;
+            }
+        }
+        return null; // Vehicle not found
+    }
+
+
+    public void completeTrip(String endTime) {
+        this.endTime = endTime;
+        this.status = "Completed";
+        String allocatedVehicleRegistrationNumber = getAllocatedVehicleRegistrationNumber();
+
+        if (allocatedVehicleRegistrationNumber != null) {
+            Vehicle allocatedVehicle = findVehicleByRegistrationNumber(allocatedVehicleRegistrationNumber);
+            if (allocatedVehicle != null) {
+                allocatedVehicle.deallocate(); // Mark the vehicle as available after the trip
+            }
+        }
+    }
+
+
+
+
+
+
+    public Object getStatus() {
+        return null;
+    }
+
+    public String getTripId() {
+        return null;
+    }
+}
